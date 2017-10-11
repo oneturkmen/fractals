548ae5e7785f936a2ebf6b67ef51efa07e8d6ea0
Batyr Nuryyev <b.g.nuryyev@gmail.com>
2017-10-11 16:46:51 -0600

    initial setup

---
+++ fractals.hs
@@ -0,0 +1,7 @@
+type Point = (Float, Float)
+
+next :: Point -> Point -> Point
+next (u, v) (x, y) = (x * x - y * y + u, 2 * x * y + v)
+
+mandelbrot :: Point -> [Point]
+mandelbrot p = iterate (next p) (0,0)
